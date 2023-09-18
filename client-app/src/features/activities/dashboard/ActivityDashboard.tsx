d632fac2e1e83892c1b1c5bab1da7e1b7be74e66
Tien Nguyen <tiennguyenmyjob@gmail.com>
2023-09-18 18:17:03 +0700

    CRUD in Client app

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Grid } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+
+interface Props {
+    activities : Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    createOrEdit: (activity : Activity)=> void;
+    deleteActivity:(id : string) => void;
+}
+
+export default function ActivityDashboard(props : Props) {
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList 
+                    activities={props.activities} 
+                    selectActivity={props.selectActivity}
+                    deleteActivity={props.deleteActivity}
+                />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {props.selectedActivity && !props.editMode &&
+                <ActivityDetails 
+                activity={props.selectedActivity} 
+                cancelActivity = {props.cancelActivity} 
+                openForm = {props.openForm}
+                />}
+                {props.editMode &&
+                <ActivityForm activity={props.selectedActivity} closeForm ={props.closeForm} createOrEdit={props.createOrEdit} />
+                }
+            </Grid.Column>
+        </Grid>
+    )
+}
